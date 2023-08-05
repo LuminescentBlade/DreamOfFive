425917af640074f8616adba294b5dfcf31be0c21
Luminescent Blade <luminescentblade@gmail.com>
2023-08-05 08:58:52 -0400

    vercel deploy works

--- next.config.js
+++ next.config.js
@@ -1,15 +1,16 @@
 /** @type {import('next').NextConfig} */
 
-let assetPrefix, basePath;
+let assetPrefix, basePath, output;
 
 if (process.env.BUILD_CONFIG === "gh") {
   console.log("building for github-pages");
   assetPrefix = process.env.BUILD_PATH;
-  basePath =  process.env.BUILD_PATH;
+  basePath = process.env.BUILD_PATH;
+  output = "export";
 }
 
 const nextConfig = {
-  output: "export",
+  output,
   assetPrefix,
   basePath,
 };
