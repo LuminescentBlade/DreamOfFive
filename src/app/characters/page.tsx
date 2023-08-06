1f03545ad84c0519c7139ab47b1428c0aac3ee88
Luminescent Blade <luminescentblade@gmail.com>
2023-08-06 11:33:28 -0400

    added local support tools

--- src/app/characters/page.tsx
+++ src/app/characters/page.tsx
@@ -1,20 +1,38 @@
 'use client';
 
-import UnitSheet from '../../components/unit-sheet';
+import { DoFArtist } from '@/src/models/enums';
+import UnitSheet from '@/src/components/unit-sheet';
 import styles from './page.module.scss'
 import html2canvas from 'html2canvas';
+import { DoFArtistConfig } from '@/src/config/artists.config';
+import { useState } from 'react';
 
 export default function CharacterPage() {
     const isProd = (process.env.NODE_ENV === 'production');
+    const [unitSheetData, updateData] = useState({
+        useProd: isProd,
+        bunny: true
+    });
+
+    if (typeof window !== "undefined") {
+        Object.values(DoFArtist).forEach(key => {
+            // @ts-ignore
+            setVariable(`--dof-artist-${key}`, DoFArtistConfig[key].color);
+        });
+    }
 
     function exportCanvas() {
         if (typeof window !== "undefined") {
             // @ts-ignore
-            html2canvas(document.querySelector("#unit-sheet")).then((canvas:HTMLCanvasElement) => {
+            html2canvas(document.querySelector("#unit-sheet")).then((canvas: HTMLCanvasElement) => {
                 save(canvas.toDataURL(), 'dof-spritesheet.png');
             });
         }
 
+    }
+
+    function setVariable(variable: string, value: string) {
+        document.documentElement.style.setProperty(variable, value);
     }
 
     function save(imageDataUrl: string, name: string) {
@@ -24,10 +42,24 @@ export default function CharacterPage() {
         tempLink.click();
         tempLink.remove();
     }
-    return (<main className={styles.base}>
-        <UnitSheet />
-        {
-            !isProd ? <button className={styles.export} onClick={exportCanvas}>Export Spritesheet</button> : ""
-        }
-    </main>);
+
+    function toggleUnitSheetProd() {
+
+        updateData({ ...unitSheetData, useProd: !unitSheetData.useProd });
+        console.log(unitSheetData);
+        console.log('owo');
+    }
+
+    return (
+        <main className={styles.base}>
+            <UnitSheet useProd={unitSheetData.useProd} />
+            {
+                !isProd ? <div style={{width: 'fit-content', margin: '12px auto'}}>
+                    <button style={{padding: '12px', height: '40px', margin: '0 12px 0 0 '}} onClick={exportCanvas}>Export Spritesheet</button>
+                    <button style={{padding: '12px', height: '40px'}} onClick={toggleUnitSheetProd}>Toggle Production Sheet</button>
+                </div> : ""
+            }
+            
+        </main>
+    );
 }
--- src/components/unit-sheet/index.tsx
+++ src/components/unit-sheet/index.tsx
@@ -7,39 +7,29 @@ import { IDoFUnit } from '@/src/models/interfaces';
 import { DoFArtist } from '@/src/models/enums';
 import { DoFArtistConfig } from '@/src/config/artists.config';
 
-export default function UnitSheet() {
-    const isProd = (process.env.NODE_ENV === 'production');
-    if (typeof window !== "undefined") {
-        Object.values(DoFArtist).forEach(key => {
-            // @ts-ignore
-            setVariable(`--dof-artist-${key}`, DoFArtistConfig[key].color);
-        });
-    }
-    function setVariable(variable: string, value: string) {
-        document.documentElement.style.setProperty(variable, value);
-    }
+export default function UnitSheet({useProd}: {useProd: boolean}) {
     const sections = Object.keys(DoFCharacters);
     const artists = Object.values(DoFArtist);
 
     return (<div id='unit-sheet' className={styles.base}>
-            {
-                // @ts-ignore
-                sections.filter(section => DoFCharacters[section].length).map((section) => (
-                    <section key={section} className={`${styles.container} ${styles.spritesheet}`}>
-                        <h2>{section}</h2>
-                        {
-                            // @ts-ignore
-                            DoFCharacters[section]
-                                .filter((character: IDoFUnit) => !character.hidden || !isProd)
-                                .map((character: IDoFUnit) => <UnitSheetSprite key={character.name} type={section} characterDef={character} />)
-                        }
-                    </section>
-                ))}
+        {
+            // @ts-ignore
+            sections.filter(section => DoFCharacters[section].length).map((section) => (
+                <section key={section} className={`${styles.container} ${styles.spritesheet}`}>
+                    <h2>{section}</h2>
+                    {
+                        // @ts-ignore
+                        DoFCharacters[section]
+                            .filter((character: IDoFUnit) => !character.hidden || !useProd)
+                            .map((character: IDoFUnit) => <UnitSheetSprite key={character.name} type={section} characterDef={character} />)
+                    }
+                </section>
+            ))}
         <section className={styles.container}>
             <h2>Artist Credits</h2>
             <ul className={styles.credits}>
                 {
-                    artists.map(artist=>(
+                    artists.map(artist => (
                         <li key={artist}>
                             <div className={styles.artist} style={{ backgroundColor: `var(--dof-artist-${artist})` }}>
                             </div>
