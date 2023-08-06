2959ce475c728affefba32e0e1f7aa02e31fadfe
Luminescent Blade <luminescentblade@gmail.com>
2023-08-06 10:08:11 -0400

    refactored out characters from page

--- src/app/characters/page.tsx
+++ src/app/characters/page.tsx
@@ -1,53 +1,8 @@
-'use client';
-
-import UnitSheetSprite from '../../components/unit-sheet-sprite';
+import UnitSheet from '../../components/unit-sheet';
 import styles from './page.module.scss'
-import { DoFCharacters } from '../../config/characters.config'
-import { IDoFUnit } from '@/src/models/interfaces';
-import { DoFArtist } from '@/src/models/enums';
-import { DoFArtistConfig } from '@/src/config/artists.config';
 
-export default function UnitSheet() {
-     const isProd = (process.env.NODE_ENV === 'production');
-     if (typeof window !== "undefined") {
-        Object.values(DoFArtist).forEach(key => {
-            // @ts-ignore
-            setVariable(`--dof-artist-${key}`, DoFArtistConfig[key].color);
-        });
-    }
-    function setVariable(variable: string, value: string) {
-        document.documentElement.style.setProperty(variable, value);
-    }
-    const sections = Object.keys(DoFCharacters);
-    const artists = Object.values(DoFArtist);
-
-    return (<main className={styles.base}>
-            {
-                // @ts-ignore
-                sections.filter(section => DoFCharacters[section].length).map((section) => (
-                    <section key={section} className={`${styles.container} ${styles.spritesheet}`}>
-                        <h2>{section}</h2>
-                        {
-                            // @ts-ignore
-                            DoFCharacters[section]
-                                .filter((character: IDoFUnit) => !character.hidden || !isProd)
-                                .map((character: IDoFUnit) => <UnitSheetSprite key={character.name} type={section} characterDef={character} />)
-                        }
-                    </section>
-                ))}
-        <section className={styles.container}>
-            <h2>Artist Credits</h2>
-            <ul className={styles.credits}>
-                {
-                    artists.map(artist=>(
-                        <li key={artist}>
-                            <div className={styles.artist} style={{ backgroundColor: `var(--dof-artist-${artist})` }}>
-                            </div>
-                            {DoFArtistConfig[artist].name}
-                        </li>
-                    ))
-                }
-            </ul>
-        </section>
+export default function CharacterPage() {
+    return (<main>
+           <UnitSheet />
     </main>);
 }
