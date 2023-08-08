14f8464eb0f0875cb5cf66947380f66b47416f91
Luminescent Blade <luminescentblade@gmail.com>
2023-08-08 17:43:56 -0400

    rm console log

--- src/app/characters/page.tsx
+++ src/app/characters/page.tsx
@@ -178,7 +178,6 @@ export default function CharacterPage() {
         }
         chapterLimit = newChapterLimit;
         update();
-        console.log(chapterLimit);
     }
 
     function toggleProd() {
