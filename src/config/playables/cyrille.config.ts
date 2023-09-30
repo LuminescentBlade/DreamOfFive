ac9b7e2bb8c8319bc3e6ac932be5e8f2014b42a1
Luminescent Blade <luminescentblade@gmail.com>
2023-09-30 01:05:02 -0400

    cyrille update

--- src/config/playables/cyrille.config.ts
+++ src/config/playables/cyrille.config.ts
@@ -13,7 +13,7 @@ export const Cyrille: IDoFPlayable = {
             player: 1
         }
     },
-    bases: { hp: 28, pow: 8, skl: 8, spd: 11, luk: 5, def: 12, res: 0, con: 9 },
+    bases: { hp: 28, pow: 8, skl: 8, spd: 11, luk: 5, def: 9, res: 0, con: 9 },
     level: 3,
     growths: { hp: 110, pow: 40, skl: 40, spd: 60, luk: 30, def: 45, res: 10 },
     isSpoiler: true,
