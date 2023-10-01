8b74920febfc8940be8729f71da89e5f9d82558d
Luminescent Blade <luminescentblade@gmail.com>
2023-09-30 22:58:10 -0400

    cyrille age

--- src/config/playables/cyrille.config.ts
+++ src/config/playables/cyrille.config.ts
@@ -14,6 +14,7 @@ export const Cyrille: IDoFCharacter = {
             player: 1
         }
     },
+    age: 23,
     bases: { hp: 28, pow: 8, skl: 8, spd: 11, luk: 5, def: 9, res: 0, con: 9 },
     level: 3,
     growths: { hp: 110, pow: 40, skl: 40, spd: 60, luk: 30, def: 45, res: 10 },
