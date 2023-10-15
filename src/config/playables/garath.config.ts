611d985eacaa1504b9b43a13af259ca03e5438d9
Luminescent Blade <luminescentblade@gmail.com>
2023-10-14 21:36:24 -0400

    taller garath

--- src/config/playables/garath.config.ts
+++ src/config/playables/garath.config.ts
@@ -14,7 +14,7 @@ export const Garath: IDoFCharacter = {
     },
     bases: {hp: 40, pow: 17, skl: 13, spd: 10, luk: 9, def: 10, res: 4, con: 18},
     level: 1,
-    height: 195,
+    height: 200,
     growths:   {hp: 75, pow: 50, skl: 40, spd: 35, luk: 30, def: 20, res: 10},
     age: 38,
     epithet: 'The Deadeye'
