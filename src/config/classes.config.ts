7bb092430b10261f3542d0684226f5ebbdd0816c
Luminescent Blade <luminescentblade@gmail.com>
2023-10-15 02:03:37 -0400

    cap change

--- src/config/classes.config.ts
+++ src/config/classes.config.ts
@@ -23,13 +23,13 @@ export const DoFUnpromotedClasses = {
         caps: {hp: 60, pow: 20, skl: 20, spd: 20, luk: 30, def: 20, res: 10, con: 20 }
     },
     [DoFClasses.Cleric]: {
-        caps: {hp: 60, pow: 20, skl: 20, spd: 20, luk: 30, def: 10, res: 20, con: 20 }
+        caps: {hp: 60, pow: 20, skl: 20, spd: 20, luk: 30, def: 15, res: 20, con: 20 }
     },
     [DoFClasses.Priest]: {
-        caps: {hp: 60, pow: 20, skl: 20, spd: 20, luk: 30, def: 10, res: 20, con: 20 }
+        caps: {hp: 60, pow: 20, skl: 20, spd: 20, luk: 30, def: 15, res: 20, con: 20 }
     },
     [DoFClasses.PegRider]: {
-        caps: {hp: 60, pow: 20, skl: 20, spd: 20, luk: 30, def: 15, res: 15, con: 20 }
+        caps: {hp: 60, pow: 20, skl: 20, spd: 20, luk: 30, def: 18, res: 18, con: 20 }
     },
     [DoFClasses.Armour]: {
         caps: {hp: 60, pow: 20, skl: 20, spd: 15, luk: 30, def: 20, res: 10, con: 20 }
@@ -41,16 +41,16 @@ export const DoFUnpromotedClasses = {
         caps: {hp: 60, pow: 20, skl: 20, spd: 20, luk: 30, def: 20, res: 10, con: 20 }
     },
     [DoFClasses.Troubadour]: {
-        caps: {hp: 60, pow: 20, skl: 20, spd: 20, luk: 30, def: 10, res: 20, con: 20 }
+        caps: {hp: 60, pow: 20, skl: 20, spd: 20, luk: 30, def: 15, res: 20, con: 20 }
     },
     [DoFClasses.Adept]: {
-        caps: {hp: 60, pow: 20, skl: 20, spd: 20, luk: 30, def: 15, res: 15, con: 20 }
+        caps: {hp: 60, pow: 20, skl: 20, spd: 20, luk: 30, def: 15, res: 20, con: 20 }
     },
     [DoFClasses.Mage]: {
-        caps: {hp: 60, pow: 20, skl: 20, spd: 20, luk: 30, def: 10, res: 20, con: 20 }
+        caps: {hp: 60, pow: 20, skl: 20, spd: 20, luk: 30, def: 15, res: 20, con: 20 }
     },
     [DoFClasses.Warlock]: {
-        caps: {hp: 60, pow: 20, skl: 20, spd: 20, luk: 30, def: 10, res: 20, con: 20 }
+        caps: {hp: 60, pow: 20, skl: 20, spd: 20, luk: 30, def: 15, res: 20, con: 20 }
     },
     [DoFClasses.DrakeRider]: {
         caps: {hp: 60, pow: 20, skl: 20, spd: 20, luk: 30, def: 20, res: 5, con: 20 }
@@ -99,7 +99,7 @@ export const DoFPromotedClasses = {
     },
     [DoFClasses.Bishop]: {
         promo: { hp: 5, pow: 3, skl: 2, spd: 3, luk: 0, def: 1, res: 3, con: 2 },
-        caps: { hp: 60, pow: 26, skl: 26, spd: 28, luk: 30, def: 15, res: 30, con: 20 }
+        caps: { hp: 60, pow: 26, skl: 26, spd: 28, luk: 30, def: 20, res: 30, con: 20 }
     },
     [DoFClasses.SeraphKnight]: {
         promo: { hp: 4, pow: 2, skl: 3, spd: 2, luk: 0, def: 2, res: 4, con: 0 },
@@ -119,7 +119,7 @@ export const DoFPromotedClasses = {
     },
     [DoFClasses.Valkyrie]: {
         promo: { hp: 4, pow: 2, skl: 3, spd: 2, luk: 0, def: 3, res: 3, con: 0 },
-        caps: { hp: 60, pow: 27, skl: 29, spd: 28, luk: 30, def: 15, res: 29, con: 20 }
+        caps: { hp: 60, pow: 27, skl: 29, spd: 28, luk: 30, def: 20, res: 29, con: 20 }
     },
     [DoFClasses.Sibyl]: {
         promo: { hp: 4, pow: 3, skl: 2, spd: 2, luk: 0, def: 3, res: 3, con: 0 },
@@ -127,11 +127,11 @@ export const DoFPromotedClasses = {
     },
     [DoFClasses.Sage]: {
         promo: { hp: 4, pow: 2, skl: 3, spd: 2, luk: 0, def: 3, res: 3, con: 1 },
-        caps: { hp: 60, pow: 27, skl: 30, spd: 27, luk: 30, def: 15, res: 28, con: 20 }
+        caps: { hp: 60, pow: 27, skl: 30, spd: 27, luk: 30, def: 20, res: 28, con: 20 }
     },
     [DoFClasses.Sorcerer]: {
         promo: { hp: 5, pow: 3, skl: 3, spd: 1, luk: 0, def: 2, res: 3, con: 2 },
-        caps: { hp: 60, pow: 30, skl: 28, spd: 25, luk: 30, def: 15, res: 29, con: 20 }
+        caps: { hp: 60, pow: 30, skl: 28, spd: 25, luk: 30, def: 20, res: 29, con: 20 }
     },
     [DoFClasses.DrakeKnight]: {
         promo: { hp: 6, pow: 3, skl: 2, spd: 3, luk: 0, def: 3, res: 0, con: 0 },
@@ -163,7 +163,7 @@ export const DoFPromotedClasses = {
     },
     [DoFClasses.Harrier]: {
         promo: { hp: 0, pow: 0, skl: 0, spd: 0, luk: 0, def: 0, res: 0, con: 0 },
-        caps: { hp: 60, pow: 26, skl: 27, spd: 28, luk: 30, def: 15, res: 25, con: 20 }
+        caps: { hp: 60, pow: 26, skl: 27, spd: 28, luk: 30, def: 20, res: 25, con: 20 }
     },
     [DoFClasses.Cataphract]: {
         promo: { hp: 0, pow: 0, skl: 0, spd: 0, luk: 0, def: 0, res: 0, con: 0 },
