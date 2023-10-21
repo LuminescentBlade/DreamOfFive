c8ca3d499e9c992a3246a61892f25cc99f5e8f77
Luminescent Blade <luminescentblade@gmail.com>
2023-10-21 19:14:51 -0400

    added simple hamburger menu

--- src/components/header/index.tsx
+++ src/components/header/index.tsx
@@ -3,11 +3,16 @@
 import Link from "next/link";
 import Toggle from "../toggle";
 import { useState, useEffect } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faClose, faXmark } from "@fortawesome/free-solid-svg-icons";
+import styles from "./index.module.scss";
+import Overlay from "../overlay";
+
 let init = false;
 export default function Header() {
     const darkModeKey = 'dof-darkmode';
-    const [isDarkMode, setDarkMode] = useState(false);
-    useEffect(()=>{
+    const [headerState, setHeaderState] = useState({ dark: false, mobileOpen: false });
+    useEffect(() => {
         const preferDark = window.matchMedia && window.matchMedia('(prefers-color-scheme: dark)').matches;
         const localDarkContent = window.localStorage.getItem(darkModeKey);
         let isCachedDark = false;
@@ -21,16 +26,16 @@ export default function Header() {
             setBodyDark(isCachedDark);
             init = true;
         }
-        if(isCachedDark !== isDarkMode){
-            setDarkMode(isCachedDark);
+        if (isCachedDark !== headerState.dark) {
+            setHeaderState({ ...headerState, dark: isCachedDark });
         }
     });
-   
+
     function toggleDarkMode() {
-        const nextDarkModeState = !isDarkMode;
+        const nextDarkModeState = !headerState;
         window.localStorage.setItem(darkModeKey, `${nextDarkModeState}`);
         setBodyDark(nextDarkModeState);
-        setDarkMode(nextDarkModeState);
+        setHeaderState({ ...headerState, dark: nextDarkModeState });
     }
 
     function setBodyDark(isDarkMode: boolean) {
@@ -41,13 +46,26 @@ export default function Header() {
         }
     }
 
+    function toggleMobileMenu() {
+        setHeaderState({ ...headerState, mobileOpen: !headerState.mobileOpen });
+    }
+
     return <>
-        <Link href="/">Home</Link>
-        <Link href="/characters">Characters</Link>
-        <Link href="/credits">Credits</Link>
+        <button className={`${styles.mobile} button-wrapper`} onClick={toggleMobileMenu}>
+            <FontAwesomeIcon icon={faBars} size="xl" />
+        </button>
+        <div className={`${styles.menu} ${headerState.mobileOpen ? styles.mobileOpen : ''}`}>
+            <button className={`${styles.mobile} button-wrapper`} onClick={toggleMobileMenu}>
+                <FontAwesomeIcon icon={faXmark} size="2x" />
+            </button>
+            <Link href="/">Home</Link>
+            <Link href="/characters">Characters</Link>
+            <Link href="/credits">Credits</Link>
+        </div>
+        {headerState.mobileOpen ? <Overlay /> : ''}
         <span className="flex-line-container flex-end">
             <span>Dark Mode</span>
-            <Toggle active={isDarkMode} onStateChange={toggleDarkMode} ariaLabel="dark mode"></Toggle>
+            <Toggle active={headerState.dark} onStateChange={toggleDarkMode} ariaLabel="dark mode"></Toggle>
         </span>
     </>
 }
