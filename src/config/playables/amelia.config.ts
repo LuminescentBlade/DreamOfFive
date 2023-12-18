d0ca0803e6861774ce710759ab94daf84771c78b
Luminescent Blade <luminescentblade@gmail.com>
2023-12-18 18:31:19 -0500

    updated con stats for the mages

--- src/config/playables/amelia.config.ts
+++ src/config/playables/amelia.config.ts
@@ -15,7 +15,7 @@ export const Amelia: IDoFCharacter = {
     height: 163,
     age: 17,
     stats: {
-        hp: 21, pow: 8, skl: 7, spd: 7, luk: 5, def: 3, res: 8, con: 7
+        hp: 21, pow: 8, skl: 7, spd: 7, luk: 5, def: 3, res: 8, con: 6
     },
     level: 1,
     growths: { hp: 55, pow: 55, skl: 45, spd: 50, luk: 45, def: 20, res: 40 },
