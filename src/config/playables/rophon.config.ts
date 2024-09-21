dd37b4d874b3b1aea03480a6181e269554e356cc
Luminescent Blade <luminescentblade@gmail.com>
2024-09-21 03:25:18 -0400

    1.6

--- src/config/playables/rophon.config.ts
+++ src/config/playables/rophon.config.ts
@@ -14,8 +14,8 @@ export const Ironside: IDoFCharacter = {
             player: 25
         }
     },
-    stats: { hp: 52, pow: 21, skl: 26, spd: 27, luk: 12, def: 17, res: 13, con: 10 },
-    growths: { hp: 255, pow: 175, skl: 220, spd: 220, luk: 1, def: 125, res: 115 },
+    stats: { hp: 57, pow: 21, skl: 26, spd: 27, luk: 12, def: 17, res: 13, con: 10 },
+    growths: { hp: 245, pow: 175, skl: 220, spd: 220, luk: 1, def: 125, res: 125 },
     level: 19,
     ranks: {
         [DoFWeaponType.Sword]: 251,
