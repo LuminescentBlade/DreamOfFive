2c5fc6388bd13496c1a6dcada6ff8322fa18c82f
Luminescent Blade <luminescentblade@gmail.com>
2024-11-21 02:23:50 -0500

    added mov to classes

--- src/config/classes.config.ts
+++ src/config/classes.config.ts
@@ -10,12 +10,14 @@ export const DoFUnpromotedClasses: { [key: string]: any } = {
     },
     [DoFClasses.Henchwoman]: {
         caps: { hp: 60, pow: 20, skl: 20, spd: 20, luk: 30, def: 20, res: 10, con: 20 },
+        mov: 7,
         weapons: {
             [DoFWeaponType.Sword]: 1
         }
     },
     [DoFClasses.Fencer]: {
         caps: { hp: 60, pow: 20, skl: 20, spd: 20, luk: 30, def: 20, res: 10, con: 20 },
+        mov: 6,
         weapons: {
             [DoFWeaponType.Sword]: 31,
             [DoFWeaponType.Lance]: 31
@@ -23,42 +25,49 @@ export const DoFUnpromotedClasses: { [key: string]: any } = {
     },
     [DoFClasses.Ironclad]: {
         caps: { hp: 60, pow: 20, skl: 20, spd: 20, luk: 30, def: 20, res: 10, con: 20 },
+        mov: 5,
         weapons: {
             [DoFWeaponType.Axe]: 31
         }
     },
     [DoFClasses.Mercenary]: {
         caps: { hp: 60, pow: 20, skl: 20, spd: 20, luk: 30, def: 20, res: 10, con: 20 },
+        mov: 6,
         weapons: {
             [DoFWeaponType.Sword]: 31
         }
     },
     [DoFClasses.Myrmidon]: {
         caps: { hp: 60, pow: 20, skl: 20, spd: 20, luk: 30, def: 20, res: 10, con: 20 },
+        mov: 6,
         weapons: {
             [DoFWeaponType.Sword]: 31
         }
     },
     [DoFClasses.Thief]: {
         caps: { hp: 60, pow: 20, skl: 20, spd: 20, luk: 30, def: 20, res: 10, con: 20 },
+        mov: 6,
         weapons: {
             [DoFWeaponType.Sword]: 31
         }
     },
     [DoFClasses.Cleric]: {
         caps: { hp: 60, pow: 20, skl: 20, spd: 20, luk: 30, def: 15, res: 20, con: 20 },
+        mov: 5,
         weapons: {
             [DoFWeaponType.Staff]: 31
         }
     },
     [DoFClasses.Priest]: {
         caps: { hp: 60, pow: 20, skl: 20, spd: 20, luk: 30, def: 15, res: 20, con: 20 },
+        mov: 5,
         weapons: {
             [DoFWeaponType.Staff]: 31
         }
     },
     [DoFClasses.PegRider]: {
         caps: { hp: 60, pow: 20, skl: 20, spd: 20, luk: 30, def: 18, res: 18, con: 20 },
+        mov: 7,
         weapons: {
             [DoFWeaponType.Sword]: 31
         },
@@ -67,24 +76,28 @@ export const DoFUnpromotedClasses: { [key: string]: any } = {
     },
     [DoFClasses.Armour]: {
         caps: { hp: 60, pow: 20, skl: 20, spd: 20, luk: 30, def: 20, res: 10, con: 20 },
+        mov: 5,
         weapons: {
             [DoFWeaponType.Lance]: 31
         }
     },
     [DoFClasses.Archer]: {
         caps: { hp: 60, pow: 20, skl: 20, spd: 20, luk: 30, def: 20, res: 10, con: 20 },
+        mov: 6,
         weapons: {
             [DoFWeaponType.Bow]: 31
         }
     },
     [DoFClasses.Fighter]: {
         caps: { hp: 60, pow: 20, skl: 20, spd: 20, luk: 30, def: 20, res: 10, con: 20 },
+        mov: 6,
         weapons: {
             [DoFWeaponType.Axe]: 31
         }
     },
     [DoFClasses.Troubadour]: {
         caps: { hp: 60, pow: 20, skl: 20, spd: 20, luk: 30, def: 15, res: 20, con: 20 },
+        mov: 8,
         weapons: {
             [DoFWeaponType.Staff]: 31
         },
@@ -92,24 +105,28 @@ export const DoFUnpromotedClasses: { [key: string]: any } = {
     },
     [DoFClasses.Adept]: {
         caps: { hp: 60, pow: 20, skl: 20, spd: 20, luk: 30, def: 15, res: 20, con: 20 },
+        mov: 5,
         weapons: {
             [DoFWeaponType.Light]: 31
         }
     },
     [DoFClasses.Mage]: {
         caps: { hp: 60, pow: 20, skl: 20, spd: 20, luk: 30, def: 15, res: 20, con: 20 },
+        mov: 5,
         weapons: {
             [DoFWeaponType.Anima]: 31
         }
     },
     [DoFClasses.Warlock]: {
         caps: { hp: 60, pow: 20, skl: 20, spd: 20, luk: 30, def: 15, res: 20, con: 20 },
+        mov: 5,
         weapons: {
             [DoFWeaponType.Dark]: 31
         }
     },
     [DoFClasses.DrakeRider]: {
         caps: { hp: 60, pow: 20, skl: 20, spd: 20, luk: 30, def: 20, res: 5, con: 20 },
+        mov: 7,
         weapons: {
             [DoFWeaponType.Lance]: 31
         },
@@ -117,18 +134,21 @@ export const DoFUnpromotedClasses: { [key: string]: any } = {
     },
     [DoFClasses.Brigand]: {
         caps: { hp: 60, pow: 20, skl: 20, spd: 20, luk: 30, def: 20, res: 10, con: 20 },
+        mov: 6,
         weapons: {
             [DoFWeaponType.Sword]: 31
         }
     },
     [DoFClasses.Raider]: {
         caps: { hp: 60, pow: 20, skl: 20, spd: 20, luk: 30, def: 20, res: 10, con: 20 },
+        mov: 6,
         weapons: {
             [DoFWeaponType.Sword]: 31
         }
     },
     [DoFClasses.Cavalier]: {
         caps: { hp: 60, pow: 20, skl: 20, spd: 20, luk: 30, def: 20, res: 10, con: 20 },
+        mov: 6,
         weapons: {
             [DoFWeaponType.Sword]: 1,
             [DoFWeaponType.Lance]: 1
@@ -137,12 +157,14 @@ export const DoFUnpromotedClasses: { [key: string]: any } = {
     },
     [DoFClasses.Soldier]: {
         caps: { hp: 60, pow: 20, skl: 20, spd: 20, luk: 30, def: 20, res: 10, con: 20 },
+        mov: 6,
         weapons: {
             [DoFWeaponType.Lance]: 31
         }
     },
     [DoFClasses.Skirmisher]: {
         caps: { hp: 60, pow: 20, skl: 20, spd: 20, luk: 30, def: 20, res: 10, con: 20 },
+        mov: 6,
         weapons: {
             [DoFWeaponType.Bow]: 31
         },
@@ -154,6 +176,7 @@ export const DoFPromotedClasses :  { [key: string]: any }  = {
     [DoFClasses.Questant]: {
         promo: { hp: 5, pow: 3, skl: 3, spd: 3, luk: 0, def: 3, res: 3, con: 1 },
         caps: { hp: 60, pow: 27, skl: 30, spd: 30, luk: 30, def: 26, res: 20, con: 20 },
+        mov: 7,
         weapons: {
             [DoFWeaponType.Sword]: 71,
             [DoFWeaponType.Axe]: 71
@@ -163,6 +186,7 @@ export const DoFPromotedClasses :  { [key: string]: any }  = {
     [DoFClasses.Blademaiden]: {
         promo: { hp: 4, pow: 1, skl: 3, spd: 2, luk: 0, def: 3, res: 8, con: 2 },
         caps: { hp: 60, pow: 26, skl: 29, spd: 27, luk: 30, def: 27, res: 30, con: 20 },
+        mov: 7,
         weapons: {
             [DoFWeaponType.Sword]: 71
         }
@@ -170,6 +194,7 @@ export const DoFPromotedClasses :  { [key: string]: any }  = {
     [DoFClasses.Hussar]: {
         promo: { hp: 5, pow: 2, skl: 3, spd: 3, luk: 0, def: 3, res: 3, con: 0 },
         caps: { hp: 60, pow: 27, skl: 29, spd: 28, luk: 30, def: 28, res: 15, con: 20 },
+        mov: 8,
         weapons: {
             [DoFWeaponType.Sword]: 71,
             [DoFWeaponType.Lance]: 71
@@ -179,6 +204,7 @@ export const DoFPromotedClasses :  { [key: string]: any }  = {
     [DoFClasses.Juggernaut]: {
         promo: { hp: 6, pow: 3, skl: 3, spd: 3, luk: 0, def: 3, res: 3, con: 2 },
         caps: { hp: 60, pow: 30, skl: 28, spd: 26, luk: 30, def: 29, res: 20, con: 20 },
+        mov: 5,
         weapons: {
             [DoFWeaponType.Axe]: 71,
             [DoFWeaponType.Lance]: 71
@@ -187,6 +213,7 @@ export const DoFPromotedClasses :  { [key: string]: any }  = {
     [DoFClasses.Enforcer]: {
         promo: { hp: 0, pow: 0, skl: 0, spd: 0, luk: 0, def: 0, res: 0, con: 0 },
         caps: { hp: 60, pow: 30, skl: 28, spd: 27, luk: 30, def: 27, res: 15, con: 20 },
+        mov: 6,
         weapons: {
             [DoFWeaponType.Sword]: 71
         }
@@ -194,6 +221,7 @@ export const DoFPromotedClasses :  { [key: string]: any }  = {
     [DoFClasses.Gladiator]: {
         promo: { hp: 4, pow: 2, skl: 3, spd: 2, luk: 0, def: 3, res: 3, con: 1 },
         caps: { hp: 60, pow: 28, skl: 27, spd: 27, luk: 30, def: 28, res: 15, con: 20 },
+        mov: 6,
         weapons: {
             [DoFWeaponType.Sword]: 31,
             [DoFWeaponType.Axe]: 31
@@ -202,6 +230,7 @@ export const DoFPromotedClasses :  { [key: string]: any }  = {
     [DoFClasses.HighPriest]: {
         promo: { hp: 5, pow: 3, skl: 2, spd: 3, luk: 0, def: 1, res: 3, con: 2 },
         caps: { hp: 60, pow: 28, skl: 26, spd: 28, luk: 30, def: 20, res: 30, con: 20 },
+        mov: 5,
         weapons: {
             [DoFWeaponType.Light]: 71,
             [DoFWeaponType.Staff]: 31
@@ -210,6 +239,7 @@ export const DoFPromotedClasses :  { [key: string]: any }  = {
     [DoFClasses.SeraphKnight]: {
         promo: { hp: 4, pow: 2, skl: 3, spd: 2, luk: 0, def: 2, res: 4, con: 0 },
         caps: { hp: 60, pow: 25, skl: 28, spd: 30, luk: 30, def: 25, res: 25, con: 20 },
+        mov: 7,
         weapons: {
             [DoFWeaponType.Sword]: 31,
             [DoFWeaponType.Lance]: 31
@@ -219,6 +249,7 @@ export const DoFPromotedClasses :  { [key: string]: any }  = {
     [DoFClasses.Dreadnought]: {
         promo: { hp: 5, pow: 3, skl: 2, spd: 1, luk: 0, def: 3, res: 3, con: 2 },
         caps: { hp: 60, pow: 29, skl: 27, spd: 25, luk: 30, def: 30, res: 15, con: 20 },
+        mov: 5,
         weapons: {
             [DoFWeaponType.Sword]: 31,
             [DoFWeaponType.Axe]: 31,
@@ -228,6 +259,7 @@ export const DoFPromotedClasses :  { [key: string]: any }  = {
     [DoFClasses.Marksman]: {
         promo: { hp: 5, pow: 2, skl: 3, spd: 1, luk: 0, def: 3, res: 3, con: 2 },
         caps: { hp: 60, pow: 29, skl: 30, spd: 26, luk: 30, def: 27, res: 15, con: 20 },
+        mov: 6,
         weapons: {
             [DoFWeaponType.Bow]: 71,
         }
@@ -235,6 +267,7 @@ export const DoFPromotedClasses :  { [key: string]: any }  = {
     [DoFClasses.Warrior]: {
         promo: { hp: 5, pow: 2, skl: 2, spd: 2, luk: 0, def: 3, res: 3, con: 3 },
         caps: { hp: 60, pow: 28, skl: 28, spd: 26, luk: 30, def: 28, res: 15, con: 20 },
+        mov: 6,
         weapons: {
             [DoFWeaponType.Bow]: 31,
             [DoFWeaponType.Axe]: 31
@@ -243,6 +276,7 @@ export const DoFPromotedClasses :  { [key: string]: any }  = {
     [DoFClasses.Valkyrie]: {
         promo: { hp: 4, pow: 2, skl: 3, spd: 2, luk: 0, def: 3, res: 3, con: 0 },
         caps: { hp: 60, pow: 27, skl: 29, spd: 28, luk: 30, def: 20, res: 29, con: 20 },
+        mov: 8,
         weapons: {
             [DoFWeaponType.Light]: 31,
             [DoFWeaponType.Staff]: 31
@@ -252,6 +286,7 @@ export const DoFPromotedClasses :  { [key: string]: any }  = {
     [DoFClasses.Sibyl]: {
         promo: { hp: 4, pow: 3, skl: 2, spd: 2, luk: 0, def: 3, res: 2, con: 1 },
         caps: { hp: 60, pow: 29, skl: 27, spd: 27, luk: 30, def: 25, res: 28, con: 20 },
+        mov: 6,
         weapons: {
             [DoFWeaponType.Light]: 31,
             [DoFWeaponType.Dark]: 31
@@ -260,6 +295,7 @@ export const DoFPromotedClasses :  { [key: string]: any }  = {
     [DoFClasses.Sage]: {
         promo: { hp: 4, pow: 2, skl: 3, spd: 2, luk: 0, def: 3, res: 3, con: 1 },
         caps: { hp: 60, pow: 27, skl: 30, spd: 27, luk: 30, def: 20, res: 28, con: 20 },
+        mov: 5,
         weapons: {
             [DoFWeaponType.Anima]: 31,
             [DoFWeaponType.Staff]: 31
@@ -268,6 +304,7 @@ export const DoFPromotedClasses :  { [key: string]: any }  = {
     [DoFClasses.Sorcerer]: {
         promo: { hp: 5, pow: 3, skl: 3, spd: 1, luk: 0, def: 2, res: 3, con: 2 },
         caps: { hp: 60, pow: 30, skl: 28, spd: 25, luk: 30, def: 20, res: 29, con: 20 },
+        mov: 5,
         weapons: {
             [DoFWeaponType.Dark]: 31,
             [DoFWeaponType.Staff]: 31
@@ -276,6 +313,7 @@ export const DoFPromotedClasses :  { [key: string]: any }  = {
     [DoFClasses.DrakeKnight]: {
         promo: { hp: 6, pow: 3, skl: 2, spd: 3, luk: 0, def: 3, res: 0, con: 0 },
         caps: { hp: 60, pow: 29, skl: 27, spd: 26, luk: 30, def: 28, res: 10, con: 20 },
+        mov: 7,
         weapons: {
             [DoFWeaponType.Lance]: 31,
             [DoFWeaponType.Axe]: 31
@@ -285,6 +323,7 @@ export const DoFPromotedClasses :  { [key: string]: any }  = {
     [DoFClasses.Rogue]: {
         promo: { hp: 5, pow: 2, skl: 2, spd: 2, luk: 0, def: 3, res: 3, con: 1 },
         caps: { hp: 60, pow: 25, skl: 26, spd: 30, luk: 30, def: 27, res: 15, con: 20 },
+        mov: 7,
         weapons: {
             [DoFWeaponType.Sword]: 71
         }
@@ -292,6 +331,7 @@ export const DoFPromotedClasses :  { [key: string]: any }  = {
     [DoFClasses.Swordmaster]: {
         promo: { hp: 5, pow: 2, skl: 3, spd: 3, luk: 0, def: 1, res: 3, con: 0 },
         caps: { hp: 60, pow: 26, skl: 29, spd: 30, luk: 30, def: 25, res: 15, con: 20 },
+        mov: 6,
         weapons: {
             [DoFWeaponType.Sword]: 71
         }
@@ -299,6 +339,7 @@ export const DoFPromotedClasses :  { [key: string]: any }  = {
     [DoFClasses.Berserker]: {
         promo: { hp: 6, pow: 3, skl: 1, spd: 2, luk: 0, def: 2, res: 3, con: 3 },
         caps: { hp: 60, pow: 29, skl: 25, spd: 29, luk: 30, def: 26, res: 15, con: 20 },
+        mov: 6,
         weapons: {
             [DoFWeaponType.Axe]: 71
         }
@@ -306,6 +347,7 @@ export const DoFPromotedClasses :  { [key: string]: any }  = {
     [DoFClasses.Paladin]: {
         promo: { hp: 5, pow: 2, skl: 2, spd: 3, luk: 0, def: 2, res: 3, con: 1 },
         caps: { hp: 60, pow: 27, skl: 26, spd: 28, luk: 30, def: 27, res: 20, con: 20 },
+        mov: 8,
         weapons: {
             [DoFWeaponType.Sword]: 31,
             [DoFWeaponType.Lance]: 31
@@ -315,6 +357,7 @@ export const DoFPromotedClasses :  { [key: string]: any }  = {
     [DoFClasses.Sentinel]: {
         promo: { hp: 4, pow: 2, skl: 2, spd: 3, luk: 0, def: 3, res: 3, con: 1 },
         caps: { hp: 60, pow: 27, skl: 29, spd: 28, luk: 30, def: 28, res: 15, con: 20 },
+        mov: 6,
         weapons: {
             [DoFWeaponType.Lance]: 71,
         }
@@ -323,6 +366,7 @@ export const DoFPromotedClasses :  { [key: string]: any }  = {
     [DoFClasses.Outrider]: {
         promo: { hp: 5, pow: 2, skl: 2, spd: 3, luk: 0, def: 2, res: 3, con: 1 },
         caps: { hp: 60, pow: 27, skl: 27, spd: 29, luk: 30, def: 26, res: 15, con: 20 },
+        mov: 8,
         weapons: {
             [DoFWeaponType.Sword]: 31,
             [DoFWeaponType.Bow]: 31
@@ -332,6 +376,7 @@ export const DoFPromotedClasses :  { [key: string]: any }  = {
     [DoFClasses.Harrier]: {
         promo: { hp: 0, pow: 0, skl: 0, spd: 0, luk: 0, def: 0, res: 0, con: 0 },
         caps: { hp: 60, pow: 26, skl: 27, spd: 28, luk: 30, def: 20, res: 25, con: 20 },
+        mov: 8,
         weapons: {
             [DoFWeaponType.Anima]: 31,
             [DoFWeaponType.Staff]: 31
@@ -341,6 +386,7 @@ export const DoFPromotedClasses :  { [key: string]: any }  = {
     [DoFClasses.Cataphract]: {
         promo: { hp: 0, pow: 0, skl: 0, spd: 0, luk: 0, def: 0, res: 0, con: 0 },
         caps: { hp: 60, pow: 28, skl: 25, spd: 26, luk: 30, def: 29, res: 15, con: 20 },
+        mov: 8,
         weapons: {
             [DoFWeaponType.Sword]: 31,
             [DoFWeaponType.Axe]: 31,
@@ -351,6 +397,7 @@ export const DoFPromotedClasses :  { [key: string]: any }  = {
     [DoFClasses.FirstLance]: {
         promo: { hp: 0, pow: 0, skl: 0, spd: 0, luk: 0, def: 0, res: 0, con: 0 },
         caps: { hp: 60, pow: 27, skl: 29, spd: 29, luk: 30, def: 26, res: 20, con: 20 },
+        mov: 8,
         weapons: {
             [DoFWeaponType.Lance]: 71
         },
@@ -358,10 +405,12 @@ export const DoFPromotedClasses :  { [key: string]: any }  = {
     },
     [DoFClasses.Dancer]: {
         caps: { hp: 60, pow: 10, skl: 10, spd: 30, luk: 30, def: 15, res: 30, con: 20 },
+        mov: 6,
         weapons: {}
     },
     [DoFClasses.Bard]: {
         caps: { hp: 60, pow: 10, skl: 10, spd: 30, luk: 30, def: 20, res: 25, con: 20 },
+        mov: 6,
         weapons: {}
     }
 }
